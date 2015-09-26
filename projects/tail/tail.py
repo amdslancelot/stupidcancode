217b62953e7f899cef5ef228e3157113a120635d
Lans Hung <lans.hung@fuhu.com>
2015-09-26 03:16:12 -0700

    -c -b -n forward & backward work

--- projects/tail/tail.py
+++ projects/tail/tail.py
@@ -1,4 +1,4 @@
-import argparse, sys, re
+import argparse, sys, re, time
 
 def toParam(s):
   if not re.match(r'(\+|\-)?[0-9]+', s):
@@ -11,7 +11,7 @@ def toParam(s):
     return -1*(int(s))
   return int(s)
 
-def init(fname, n, buf_size=128):
+def tail_line(fname, n, buf_size=128):
   buf = ""
   pos = 0
   block_count = 0
@@ -68,9 +68,9 @@ def init(fname, n, buf_size=128):
       return buf
   return buf
       
-
-def init2(fname, n, buf_size=128):
+def tail_block(fname, n, buf_size=512):
   buf = ""
+  pos = 0
   block_count = 0
   direction = 1
 
@@ -82,30 +82,48 @@ def init2(fname, n, buf_size=128):
     if n >= 0:
       fh.seek(0,0)
       direction = 1
+      pos = 0
     else:
       fh.seek(0,2)
       direction = -1
+      pos = file_size
     n = abs(n)
 
-    while block_count <= n and file_size > 0:
+    while block_count < n and file_size > 0:
       if buf_size > file_size:
         buf_size = file_size
-      
-      fh.seek(direction*buf_size, 1)
-      buf_read = fh.read(buf_size)
-      fh.seek(direction*buf_size, 1)
-      
-      block_count += buf_read.count("\n")
-      file_size -= buf_size
-      buf = buf_read + buf
 
-  count = block_count - n
-  pos = 0
-  while count > 0:
-    pos = buf.find("\n")
-    buf = buf[pos+1:]
-    count -= 1
+      if direction == 1:
+        buf_read = fh.read(buf_size)
+      else:
+        fh.seek(direction*buf_size, 1)
+        buf_read = fh.read(buf_size)
+        fh.seek(direction*buf_size, 1)
+
+      block_count += 1
+      pos = fh.tell()
+      
+      if direction == 1:
+        buf = buf + buf_read
+      else:
+        buf = buf_read + buf
+
+    if direction == 1:
+      fh.seek(pos+1,0)
+      return fh.read()
+    else:
+      return buf
   return buf
+
+def follow(fname):
+  with open(fname, 'r') as fh:
+    fh.seek(0,2)
+    while True:
+      new = fh.read()
+      print "new:", new
+      if new:
+        sys.stdout.write(new)
+      time.sleep(1)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(prog="Tail", description="The tail utility displays the contents of file or, by default, its standard input, to the standard output.")
@@ -113,26 +131,32 @@ if __name__ == "__main__":
   parser.add_argument("fname", help="The file name as input")
   parser.add_argument("-f", action="store_true",
                       help="The -f option causes tail to not stop when end of file is reached, but rather to wait for additional data to be appended to the input.")
-  parser.add_argument("-b", type=toParam, default="512", nargs='?',
+  parser.add_argument("-b", type=toParam, nargs='?',
                       help="The location is number 512-byte blocks.")
-  parser.add_argument("-c", type=toParam, default="0", nargs='?',
+  parser.add_argument("-c", type=toParam, nargs='?',
                       help="The location is number bytes.")
-  parser.add_argument("-n", type=toParam, default="10", nargs='?',
+  parser.add_argument("-n", type=toParam, nargs='?',
                       help="The location is number lines.")
 
   args = parser.parse_args()
   if args.fname:
     print "fname:", args.fname
-  if args.f:
-    print "f:", args.f
+  
   if args.b:
     print "b:", args.b
-  if args.c:
+    r = tail_block(args.fname, args.b)
+  elif args.c:
     print "c:", args.c
-  if args.n:
+    r = tail_block(args.fname, args.c, buf_size=1)
+  elif args.n:
     print "n:", args.n
+    r = tail_line(args.fname, args.n)
   else:
     print "error"
   
-  r = init(args.fname, args.n)
   sys.stdout.write(r)
+  
+  if args.f:
+    print "f:", args.f
+    follow(args.fname)
+  
